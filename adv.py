d98b628314b71e85afb4b4b253c34962c8d0e3ac
iesous-kurios <j.wylie.81@gmail.com>
2020-11-22 15:48:48 -0800

    mvp

--- adv.py
+++ adv.py
@@ -4,6 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
+from typing import List
 
 # Load world
 world = World()
@@ -26,9 +27,63 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
+
 traversal_path = []
 
+# copy paste from utils from graphs 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value) -> None:
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self) -> int:
+        return len(self.stack)
+    def peek(self):
+        return self.stack[-1]
+
+# dictionary to hold inverse directions for backing up part
+reverse_directions = {'n':'s', 's':'n', 'w':'e', 'e':'w'}
+
+def traverse_graph() -> List[str]:
+    traversal_path = []
+    # hold rooms and the path to the rooms
+    s = Stack()
+
+    # store k,v pairs of rooms and path to rooms
+    visited_graph = {}
+    s.push(value=(player.current_room, None))
+    while s.size() > 0:
+        curr = s.peek()
+        room, direction_traveled = curr[0], curr[1]
+        if room.id not in visited_graph:
+            visited_graph[room.id] = set() # initial set of directions we've gone, use set to avoid duplicates
+        if direction_traveled:
+            visited_graph[room.id].add(direction_traveled)
+        # break if we visit all the rooms
+        if len(visited_graph) == len(room_graph):
+            break
+        # Use depth first search to get unexplored branches
+        unexplored_branches = [direction for direction in room.get_exits() if direction not in visited_graph[room.id]]
+        if unexplored_branches:
+            random_direction = random.choice(unexplored_branches)
+            print("Found at least one possible direction, choosing at random if more than one is found:",random_direction)
+            visited_graph[room.id].add(random_direction)
+            # push the room and path to room
+            s.push(value=(room.get_room_in_direction(random_direction), reverse_directions[random_direction]))
+            traversal_path.append(random_direction)
+        # when at dead end, begin backing up to spot with untraveled directions
+        else:
+            print("Reached dead-end, backing up to directions not yet traveled by traveling:", direction_traveled)
+            traversal_path.append(direction_traveled)
+            s.pop()
+    return traversal_path
+
+traversal_path = traverse_graph()
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -51,7 +106,7 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
+"""player.current_room.print_room_description(player)
 while True:
     cmds = input("-> ").lower().split(" ")
     if cmds[0] in ["n", "s", "e", "w"]:
@@ -59,4 +114,4 @@ while True:
     elif cmds[0] == "q":
         break
     else:
-        print("I did not understand that command.")
+        print("I did not understand that command.")"""
